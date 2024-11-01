b5db2d703f2b6643590df482b26901be1f6264a4
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-11-01 09:42:16 +0100

    Indent fix

--- Onboard_computer.py
+++ Onboard_computer.py
@@ -168,33 +168,33 @@ class MotionData:
         self.temp_z=yaw
         
         return [self.temp_x,self.temp_y,self.temp_z]
-    @property
+    #@property
     def upwards(self,signal_up):
     	duty=100
     	return duty*signal_up
-    @property
-	def downwards(self,signal_down):
-		duty=100
-		return duty/signal_down
-	@property
-	def straight(self,signal_straight):
-		duty=100
-		servo=2
-		return [duty*signal_straight,servo]
-	@property
-	def backwards(self,signal_back):
-		duty=100
-		servo=1
-		return [duty/signal_back,servo]
-	@property
-	def rotate_l(self, signal_l):
-		duty=100
-		servo1=1.25
-		servo2=1.75
-		return [duty*signal_l,servo1,servo2]
-	@property
-	def rotate_r(self,signal_r):
-		duty=100
-		servo1=1.75
-		servo2=1.25
-		return [duty*signal_l,servo1,servo2]
+    #@property
+    def downwards(self,signal_down):
+	duty=100
+	return duty/signal_down
+	#@property
+    def straight(self,signal_straight):
+	duty=100
+	servo=2
+	return [duty*signal_straight,servo]
+	#@property
+    def backwards(self,signal_back):
+	duty=100
+	servo=1
+	return [duty/signal_back,servo]
+	#@property
+    def rotate_l(self, signal_l):
+	duty=100
+	servo1=1.25
+	servo2=1.75
+	return [duty*signal_l,servo1,servo2]
+	#@property
+    def rotate_r(self,signal_r):
+	duty=100
+	servo1=1.75
+	servo2=1.25
+	return [duty*signal_l,servo1,servo2]
