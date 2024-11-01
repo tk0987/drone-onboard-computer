2b61971dacc7f450b572934427215bfa450ad844
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-11-01 08:48:52 +0100

    Tiltrotor movements complete. Bug checkin now

--- Onboard_computer.py
+++ Onboard_computer.py
@@ -109,28 +109,28 @@ class MotionData:
     	duty=100
     	return duty*signal_up
     @property
-    def downwards(self,signal_down):
-	duty=100
-	return duty/signal_down
-    @property
-    def straight(self,signal_straight):
-	duty=100
-	servo=2
-	return [duty*signal_straight,servo]
-    @property
-    def backwards(self,signal_back):
-	duty=100
-	servo=1
-	return [duty/signal_back,servo]
-    @property
-    def rotate_l(self, signal_l):
-	duty=100
-	servo1=1.25
-	servo2=1.75
-	return [duty*signal_l,servo1,servo2]
-    @property
-    def rotate_r(self,signal_r):
-	duty=100
-	servo1=1.75
-	servo2=1.25
-	return [duty*signal_l,servo1,servo2]
+	def downwards(self,signal_down):
+		duty=100
+		return duty/signal_down
+	@property
+	def straight(self,signal_straight):
+		duty=100
+		servo=2
+		return [duty*signal_straight,servo]
+	@property
+	def backwards(self,signal_back):
+		duty=100
+		servo=1
+		return [duty/signal_back,servo]
+	@property
+	def rotate_l(self, signal_l):
+		duty=100
+		servo1=1.25
+		servo2=1.75
+		return [duty*signal_l,servo1,servo2]
+	@property
+	def rotate_r(self,signal_r):
+		duty=100
+		servo1=1.75
+		servo2=1.25
+		return [duty*signal_l,servo1,servo2]
